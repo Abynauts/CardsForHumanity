259157dd1e8b1b7ad8ba62c160d0e6eaf1d671de
Sean Mercer <60895318+NotSean725@users.noreply.github.com>
2023-02-27 14:58:20 -0600

    Figured out matchmaking
    - Added Advanced Sessions Plugin
    - Adjusted various elements in DefaultEngine.ini file to get matchmaking to work
    - Started on random name for server

---
+++ Source/CardsForHumanity/Public/AdvancedSessions_Code.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AdvancedSessions_Code.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CARDSFORHUMANITY_API UAdvancedSessions_Code : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
